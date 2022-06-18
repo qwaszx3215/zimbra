b7b7a7b84c174c6f03adbe981950147a6f4facdc
jeffxson <65169599+jeffxson@users.noreply.github.com>
2022-06-18 15:08:19 +0600

    firstmmit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
-import React from "react"; 
-import { Routes, Route } from "react-router-dom"
-import Forms from "./form" 
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Forms from "./form";
 
 function App() {
- 
-
   return (
-   <div className="App">
+    <div className="App">
       <Routes>
-        <Route path="/:id" element={ <Forms/> } /> 
-        <Route path="/" element={ <Forms/> } /> 
+        <Route path="/:id" element={<Forms />} />
       </Routes>
     </div>
   );
