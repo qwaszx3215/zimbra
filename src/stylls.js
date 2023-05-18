913f4dbf284130dc5601c754af875622145995a5
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2023-05-18 13:42:59 +0100

    Update stylls.js

--- src/stylls.js
+++ src/stylls.js
@@ -1,27 +1,60 @@
 import styled from "styled-components";
 
 export const Styls = styled.div`
-  .contsainer {
-    
-    font-family: "Open Sans",helvetica,arial,sans-serif;
-    @media (max-width: 920px) {
-      width: 70%;
-    }
+font-family: "Helvetica Neue",Helvetica,Arial,"Liberation Sans",sans-serif;
+font-size: 12px;
+@media (max-width: 920px) {
+  width: 70%;
+}
+.inner{
+  margin: auto;
+  width:66%;
+  margin-top: -20px;
+  .stay{ 
+     width:60%;
+     margin-left: 80px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .stay2{ 
+    width:70%;
     margin: auto;
-    margin-top: 130px;
-    padding-left: 80px;
-    width: 30%;
+    margin-top: 20px;
+    padding-bottom: 60px;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+select{
+  margin-right: 20px;
+}
+margin: auto;
+margin-top: 130px;
+padding-left: 80px;
+width: 40%;
+  .formal{
+    background:#007CC3;
+    margin: auto;
+    width: 95%;
+    color:white;
+  }
+  .labcon{
+    display: flex;
+    span{
+      margin: 13px;
+      margin-top: 12px
+    }
   }
   .imagee {
-    width: 75%;
-    margin-bottom: 35px;
-    margin-left: -5px;
+    width: 200px;
+    margin: 35px;
+ 
   }
   label {
     font-family: "Open Sans",helvetica,arial,sans-serif;
-    font-size: 15px;
+    font-size: 13px;
     span {
-      font-weight: 600;
+      font-weight: 100;
       color: #435c72;
     }
   }
@@ -42,49 +75,50 @@ export const Styls = styled.div`
   }
 
   input[type="email"] {
-    font-size: 13.7px;
+    font-size: 12px;
     font-family: "Open Sans",helvetica,arial,sans-serif;
     width: 60%;
-    height: 18px;
-    padding-left: 40px;
-    font-weight: 500;
-    padding-top: 8.5px;
-    padding-bottom: 7.5px;
-    margin-top: 10px;
-    margin-bottom: 14px;
+    height: auto;
+    padding-left: 4px;
+    font-weight: 100;
+    padding-top: 4px;
+    padding-bottom: 3px;
+    margin-top: 5px;
+    margin-bottom: 8px;
     border: 2px solid #bebebe;
     border-radius: 4px;
   }
   input[type="password"] {
-    font-size: 13.7px;
+    font-size: 12px;
     font-family: "Open Sans",helvetica,arial,sans-serif;
     width: 60%;
-    height: 18px;
-    padding-left: 40px;
-    font-weight: 500;
-    padding-top: 8.5px;
-    padding-bottom: 7.5px;
-    margin-top: 10px;
+    height: auto;
+    padding-left: 4px;
+    font-weight: 100;
+    padding-top: 4px;
+    padding-bottom: 3px;
+    margin-top: 5px;
+    margin-bottom: 8px;
+    margin-left: 3px;
     border: 2px solid #bebebe;
     border-radius: 4px;
   }
 
   button {
-    font-size: 14px;
+    font-size: 12px;
     font-family: "Open Sans",helvetica,arial,sans-serif;
-    width: 72%;
-    font-weight: 600;
+    width: auto;
     color: white;
     cursor: pointer;
     height: auto;
     padding: 2px 0px;
     text-align: center;
-    padding-top: 9px;
-    padding-bottom: 9px;
-    margin-top: 30px;
-    border: 1px solid rgb(105, 103, 103);
-    background: #179bd7;
-    border-radius: 7px 6px 6px 7px;
+    padding: 9px; 
+    padding-top: 5px; 
+    padding-bottom: 5px; 
+    border: 1px solid white;
+    background: transparent;
+    border-radius:3px;
   }
   .reda {
     color: red;
@@ -106,13 +140,13 @@ export const Styls = styled.div`
     a{
       font-size: 13.4px;
       font-family: "Open Sans",helvetica,arial,sans-serif;
-      font-weight: 600;
+      font-weight: 100;
       color: rgb(107, 97, 97);
       text-decoration: none;
     }
   }
 .smaller{
-  margin-top: 100px;
+  margin-top: 250px;
 text-align: center;
   font-family: "Open Sans",helvetica,arial,sans-serif;
   font-size: 11px;
